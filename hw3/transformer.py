da2116771d3053764b338ab712f69038f024fdf9
itamar3113 <itamar3113@gmail.com>
2024-08-20 13:59:18 +0300

    sliding window attention

--- hw3/transformer.py
+++ hw3/transformer.py
@@ -4,63 +4,68 @@ import math
 
 
 
-
+import torch
 
 def sliding_window_attention(q, k, v, window_size, padding_mask=None):
     '''
-    Computes the simple sliding window attention from 'Longformer: The Long-Document Transformer'.
-    This implementation is meant for multihead attention on batched tensors. It should work for both single and multi-head attention.
-    :param q - the query vectors. #[Batch, SeqLen, Dims] or [Batch, num_heads, SeqLen, Dims]
-    :param k - the key vectors.  #[Batch, *, SeqLen, Dims] or [Batch, num_heads, SeqLen, Dims]
-    :param v - the value vectors.  #[Batch, *, SeqLen, Dims] or [Batch, num_heads, SeqLen, Dims]
-    :param window_size - size of sliding window. Must be an even number.
-    :param padding_mask - a mask that indicates padding with 0.  #[Batch, SeqLen]
-    :return values - the output values. #[Batch, SeqLen, Dims] or [Batch, num_heads, SeqLen, Dims]
-    :return attention - the attention weights. #[Batch, SeqLen, SeqLen] or [Batch, num_heads, SeqLen, SeqLen]
+    Computes the sliding window attention.
+    :param q: the query vectors. #[Batch, SeqLen, Dims] or [Batch, SeqLen, Dims]
+    :param k: the key vectors.  #[Batch, *, SeqLen, Dims] or [Batch, SeqLen, Dims]
+    :param v: the value vectors.  #[Batch, *, SeqLen, Dims] or [Batch, SeqLen, Dims]
+    :param window_size: size of sliding window. Must be an even number.
+    :param padding_mask: a mask that indicates padding with 0.  #[Batch, SeqLen]
+    :return values: the output values. #[Batch, *, SeqLen, Dims] or [Batch, SeqLen, Dims]
+    :return attention: the attention weights. #[Batch, *, SeqLen, SeqLen] or [Batch, SeqLen, SeqLen]
     '''
-    assert window_size%2 == 0, "window size must be an even number"
+    assert window_size % 2 == 0, "window size must be an even number"
     seq_len = q.shape[-2]
     embed_dim = q.shape[-1]
-    batch_size = q.shape[0] 
+    batch_size = q.shape[0]
 
-    values, attention = None, None
+    if len(q.shape) == 3:
+        q = q.unsqueeze(1)
+        k = k.unsqueeze(1)
+        v = v.unsqueeze(1)
 
-    # TODO:
-    #  Compute the sliding window attention.
-    # NOTE: We will not test your implementation for efficiency, but you are required to follow these two rules:
-    # 1) Implement the function without using for loops.
-    # 2) DON'T compute all dot products and then remove the uneccessary comptutations
-    #    (You can compute the dot products for any entry, even if it corresponds to padding, as long as it is within the window).
-    # Aside from these two rules, you are free to implement the function as you wish. 
-    ## HINT: There are several ways to implement this function, and while you are free to implement it however you may wish,
-    ## some are more intuitive than others. We suggest you to consider the following:
-    ## Think how you can obtain the indices corresponding to the entries in the sliding windows using tensor operations (without loops),
-    ## and then use these indices to compute the dot products directly.
-    # ====== YOUR CODE: ======
-    raise NotImplementedError()
-    # ========================
-
+    heads_dim = q.shape[1]
+    sliding_mask = torch.zeros(seq_len, seq_len, device=q.device).float()
+    lower_triangular = torch.tril(torch.ones((seq_len, seq_len), device=q.device), window_size // 2)
+    upper_triangular = torch.triu(torch.ones((seq_len, seq_len), device=q.device), -window_size // 2)
+    sliding_mask = lower_triangular * upper_triangular
+    sliding_mask = sliding_mask.unsqueeze(0).unsqueeze(0)
+    pre_norm_attention = torch.matmul(q, k.transpose(-2, -1)) / (embed_dim ** 0.5)
+    pre_norm_attention = pre_norm_attention.masked_fill(sliding_mask == 0, float("-inf"))
+    if padding_mask is not None:
+        padding_mask = padding_mask.unsqueeze(1).unsqueeze(-1)
+        pre_norm_attention = pre_norm_attention.masked_fill(~padding_mask, float("-inf"))
+    attention = torch.softmax(pre_norm_attention, dim=-1)
+    values = torch.matmul(attention, v)
+    if len(q.shape) == 4: 
+        attention = attention.squeeze(1)
+        values = values.squeeze(1)
 
     return values, attention
 
 
 
+
+
 class MultiHeadAttention(nn.Module):
-    
+
     def __init__(self, input_dim, embed_dim, num_heads, window_size):
         super().__init__()
         assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
-        
+
         self.embed_dim = embed_dim
         self.num_heads = num_heads
         self.head_dim = embed_dim // num_heads
         self.window_size = window_size
-        
+
         # Stack all weight matrices 1...h together for efficiency
         # "bias=False" is optional, but for the projection we learned, there is no teoretical justification to use bias
         self.qkv_proj = nn.Linear(input_dim, 3*embed_dim)
         self.o_proj = nn.Linear(embed_dim, embed_dim)
-        
+
         self._reset_parameters()
 
     def _reset_parameters(self):
@@ -73,13 +78,13 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x, padding_mask, return_attention=False):
         batch_size, seq_length, embed_dim = x.size()
         qkv = self.qkv_proj(x)
-        
+
         # Separate Q, K, V from linear output
         qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3*self.head_dim)
         qkv = qkv.permute(0, 2, 1, 3) # [Batch, Head, SeqLen, 3*Dims]
-        
+
         q, k, v = qkv.chunk(3, dim=-1) #[Batch, Head, SeqLen, Dims]
-        
+
         # Determine value outputs
         # TODO:
         # call the sliding window attention function you implemented
@@ -90,16 +95,16 @@ class MultiHeadAttention(nn.Module):
         values = values.permute(0, 2, 1, 3) # [Batch, SeqLen, Head, Dims]
         values = values.reshape(batch_size, seq_length, embed_dim) #concatination of all heads
         o = self.o_proj(values)
-        
+
         if return_attention:
             return o, attention
         else:
             return o
-        
-        
+
+
 class PositionalEncoding(nn.Module):
 
-    def __init__(self, d_model, max_len=5000): 
+    def __init__(self, d_model, max_len=5000):
         """
         Inputs
             d_model - Hidden dimensionality of the input.
@@ -114,17 +119,17 @@ class PositionalEncoding(nn.Module):
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)
-        
+
         # register_buffer => Tensor which is not a parameter, but should be part of the modules state.
         # Used for tensors that need to be on the same device as the module.
-        # persistent=False tells PyTorch to not add the buffer to the state dict (e.g. when we save the model) 
+        # persistent=False tells PyTorch to not add the buffer to the state dict (e.g. when we save the model)
         self.register_buffer('pe', pe, persistent=False)
 
     def forward(self, x):
         x = x + self.pe[:, :x.size(1)]
         return x
-    
-    
+
+
 
 class PositionWiseFeedForward(nn.Module):
     def __init__(self, input_dim, hidden_dim):
@@ -136,7 +141,7 @@ class PositionWiseFeedForward(nn.Module):
     def forward(self, x):
         return self.fc2(self.activation(self.fc1(x)))
 
-    
+
 class EncoderLayer(nn.Module):
     def __init__(self, embed_dim, hidden_dim, num_heads, window_size, dropout=0.1):
         '''
@@ -152,7 +157,7 @@ class EncoderLayer(nn.Module):
         self.norm1 = nn.LayerNorm(embed_dim)
         self.norm2 = nn.LayerNorm(embed_dim)
         self.dropout = nn.Dropout(dropout)
-        
+
     def forward(self, x, padding_mask):
         '''
         :param x: the input to the layer of shape [Batch, SeqLen, Dims]
@@ -168,11 +173,11 @@ class EncoderLayer(nn.Module):
         # ====== YOUR CODE: ======
         raise NotImplementedError()
         # ========================
-        
+
         return x
-    
-    
-    
+
+
+
 class Encoder(nn.Module):
     def __init__(self, vocab_size, embed_dim, num_heads, num_layers, hidden_dim, max_seq_length, window_size, dropout=0.1):
         '''
@@ -213,16 +218,16 @@ class Encoder(nn.Module):
         #  2) Apply positional encoding to the output of step 1.
         #  3) Apply a dropout layer to the output of the positional encoding.
         #  4) Apply the specified number of encoder layers.
-        #  5) Apply the classification MLP to the output vector corresponding to the special token [CLS] 
+        #  5) Apply the classification MLP to the output vector corresponding to the special token [CLS]
         #     (always the first token) to receive the logits.
         # ====== YOUR CODE: ======
         raise NotImplementedError()
-        
+
         # ========================
-        
-        
-        return output  
-    
+
+
+        return output
+
     def predict(self, sentence, padding_mask):
         '''
         :param sententence #[Batch, max_seq_len]
@@ -233,4 +238,3 @@ class Encoder(nn.Module):
         preds = torch.round(torch.sigmoid(logits))
         return preds
 
-    
